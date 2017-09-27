c9a5e917553bde16584aeb590c62d9be363fa1dd
kevingilfether <kevingilfether@gmail.com>
2017-09-27 15:02:45 -0400

    First commit

---
+++ Week1_day3HW/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_day3HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conditional Practice
+            //#1 - checks if integers are equal
+
+
+        }
+    }
+}
